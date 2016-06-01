238cadc87af1f9ca2e91da257ba1c08dd949e1d0
pablofv <tramitesyregistraciones@gmail.com>
2016-06-01 17:02:33 -0300

    Pedido fores
    Cambio en la forma de calcular los ingresados (agregamos tipo de oficina
    igual a uno o dos).
    Agregamos que el resultado se muestre con número de artículo, inciso y
    apertura de artículo.

--- PEDIDO_FORES.sql
+++ PEDIDO_FORES.sql
@@ -15,7 +15,7 @@ Aclaración: como una causa puede tener mas de un delito, se informa cantidad de 
 create table est_pedido_fores as
 SELECT *
 from (select ROW_NUMBER() over(partition by c.ID_EXPEDIENTE,o2.ID_OFICINA order by FECHA_ASIGNACION ) rn,
-          --   OFICINA_RADICACION_MOD(c.ID_CAMBIO_ASIGNACION_EXP,c.FECHA_ASIGNACION,c.ID_EXPEDIENTE,c.ID_OFICINA,c.ID_SECRETARIA) OFICINA_RADICACION,
+            -- OFICINA_RADICACION_MOD(c.ID_CAMBIO_ASIGNACION_EXP,c.FECHA_ASIGNACION,c.ID_EXPEDIENTE,c.ID_OFICINA,c.ID_SECRETARIA) OFICINA_RADICACION,
              c.*,
              o2.DESCRIPCION JUZGADO,
              o2.SIGLA_CEDULAS,
@@ -37,6 +37,7 @@ from (select ROW_NUMBER() over(partition by c.ID_EXPEDIENTE,o2.ID_OFICINA order 
       and o.ID_TIPO_INSTANCIA = 1
       and (o.ID_CAMARA  not in (9, 99) OR (o.ID_CAMARA = 9 and o2.SIGLA_CEDULAS in ('CI', 'CR','JNM', 'RO'))
           )
+      and o.ID_TIPO_OFICINA in (1,2)    
      )
 where rn = 1
   and trunc(FECHA_ASIGNACION) between to_date('20150101', 'YYYYMMDD') and to_date('20151231', 'YYYYMMDD')
@@ -52,13 +53,13 @@ order by ID_CAMARA, JUZGADO
 /* resultado en MCENTRA 1109 registros */
 /* Hoy 1ero Junio me pidió Inés que incluya los dos artículos que habías excluído, así que voy a eliminar la restricción de id 566 y 567 */
 
-select d.DESCRIPCION_DELITO, /*d.articulo, d.inciso, */p.ID_CAMARA, count(*) -- id_cambio_asignacion_exp, count(*)
+select p.ID_CAMARA, d.articulo, d.apertura_articulo, d.inciso, d.DESCRIPCION_DELITO, count(*) -- id_cambio_asignacion_exp, count(*)
 from EST_PEDIDO_FORES p JOIN DELITO_EXPEDIENTE de on p.ID_EXPEDIENTE = de.ID_EXPEDIENTE
                         join delito d on de.ID_DELITO = d.ID_DELITO
 where (d.articulo in (256, 257, 258, 259, 260, 261, 265, 266, 267, 268)) or ((articulo in (173) and inciso in (7)) or ((articulo in (174) and inciso in (5))))
 -- group by p.ID_CAMBIO_ASIGNACION_EXP
-group by p.id_camara, d.descripcion_delito--, d.articulo, d.inciso
-order by /*4,2,3 */ p.id_camara, d.descripcion_delito
+group by d.articulo, d.apertura_articulo, d.inciso, d.DESCRIPCION_DELITO, p.ID_CAMARA
+order by /*4,2,3 */ p.id_camara, d.articulo, d.apertura_articulo nulls first, d.inciso, d.DESCRIPCION_DELITO
 ;
 
 /* Para saber cuantos ingresos diferentes son */
